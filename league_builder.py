d72aaa346f48f414538eda3c542a63bd489fa5fc
Fendrel- <dswift88@gmail.com>
2018-10-10 00:46:41 -0700

    Project 1 - Soccer League
    Treehouse Techdegree Project 1 - Soccer League Coordinator

---
+++ league_builder.py
@@ -0,0 +1,42 @@
+if __name__ == "__main__":
+    import csv
+
+    def team_splitter(players, sharks, dragons, raptors):
+        while players:
+            sharks.append(players.pop())
+            dragons.append(players.pop())
+            raptors.append(players.pop())
+        return sharks, dragons, raptors
+
+    with open('soccer_players.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        players = []
+        for player in reader:
+            players.append({"Name": player['Name'],
+                            "Experienced": player['Soccer Experience'],
+                            "Guardian Name(s)": player['Guardian Name(s)']
+                            })
+
+    experienced_players = []
+    inexperienced_players = []
+    for player in players:
+        if player['Experienced'] == "YES":
+            experienced_players.append(player)
+        else:
+            inexperienced_players.append(player)
+
+    sharks = []
+    dragons = []
+    raptors = []
+    sharks, dragons, raptors = team_splitter(experienced_players,
+                                             sharks,
+                                             dragons,
+                                             raptors)
+    sharks, dragons, raptors = team_splitter(inexperienced_players,
+                                             sharks,
+                                             dragons,
+                                             raptors)
+
+    print("Sharks: {}".format(sharks), end="\n\n")
+    print("Dragons: {}".format(dragons), end="\n\n")
+    print("Raptors: {}".format(raptors), end="\n\n")
