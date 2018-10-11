18079f3c970f493df6584b1cbe8d836de9f45564
Fendrel- <dswift88@gmail.com>
2018-10-10 23:02:57 -0700

    Update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -1,42 +1,60 @@
-if __name__ == "__main__":
+if __name__ == '__main__':
     import csv
 
-    def team_splitter(players, sharks, dragons, raptors):
-        while players:
-            sharks.append(players.pop())
-            dragons.append(players.pop())
-            raptors.append(players.pop())
+    def team_splitter(experienced_players, inexperienced_players):
+        '''Takes a list of experienced players and a list of inexperienced
+           players and divides them up in round-robin fashion and returns a
+           list of players for each team.
+           '''
+        def split(players):
+            while players:
+                sharks.append(players.pop())
+                dragons.append(players.pop())
+                raptors.append(players.pop())
+
+        sharks, dragons, raptors = [], [], []
+        split(experienced_players)
+        split(inexperienced_players)
         return sharks, dragons, raptors
 
+    def team_writer(team, players):
+        '''Takes a list of players and their corresponding team name and
+           appends them to teams.txt
+           '''
+        with open('teams.txt', 'a') as file:
+            file.write('{}\n'.format(team))
+            for player in players:
+                file.write('{}, {}, {}\n'.format(player['Name'],
+                                                 player['Experienced'],
+                                                 player['Guardian Name(s)']))
+            file.write('\n')
+
+    '''Initializes the players variable, reads the csv file and appends the list
+       of players to the players variable.
+       '''
     players = []
     with open('soccer_players.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for player in reader:
-            players.append({"Name": player['Name'],
-                            "Experienced": player['Soccer Experience'],
-                            "Guardian Name(s)": player['Guardian Name(s)']
+            players.append({'Name': player['Name'],
+                            'Experienced': player['Soccer Experience'],
+                            'Guardian Name(s)': player['Guardian Name(s)']
                             })
 
+    '''Initializes variables for experienced and
+       inexperienced players then iterates through the list
+       of players and assigns the players accordingly.
+       '''
     experienced_players = []
     inexperienced_players = []
     for player in players:
-        if player['Experienced'] == "YES":
+        if player['Experienced'] == 'YES':
             experienced_players.append(player)
         else:
             inexperienced_players.append(player)
 
-    sharks = []
-    dragons = []
-    raptors = []
     sharks, dragons, raptors = team_splitter(experienced_players,
-                                             sharks,
-                                             dragons,
-                                             raptors)
-    sharks, dragons, raptors = team_splitter(inexperienced_players,
-                                             sharks,
-                                             dragons,
-                                             raptors)
-
-    print("Sharks: {}".format(sharks), end="\n\n")
-    print("Dragons: {}".format(dragons), end="\n\n")
-    print("Raptors: {}".format(raptors), end="\n\n")
+                                             inexperienced_players)
+    team_writer('Sharks', sharks)
+    team_writer('Dragons', dragons)
+    team_writer('Raptors', raptors)
