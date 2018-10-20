34c92ac13ffca2f9caa274df6ac6912454ea8e4e
Fendrel- <dswift88@gmail.com>
2018-10-19 17:44:19 -0700

    update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -1,36 +1,54 @@
-if __name__ == '__main__':
-    import csv
+import csv
 
-    def team_splitter(experienced_players, inexperienced_players):
-        '''Takes a list of experienced players and a list of inexperienced
-        players and divides them up in round-robin fashion and returns a
-        list of players for each team.
-        '''
-        def split(players):
-            while players:
-                sharks.append(players.pop())
-                dragons.append(players.pop())
-                raptors.append(players.pop())
 
-        sharks, dragons, raptors = [], [], []
-        split(experienced_players)
-        split(inexperienced_players)
-        return sharks, dragons, raptors
+def team_splitter(experienced_players, inexperienced_players):
+    '''Takes a list of experienced players and a list of inexperienced
+    players and divides them up in round-robin fashion and returns a
+    list of players for each team.
+    '''
+    def assign_team(players):
+            try:
+                global last_added
+                if last_added == raptors:
+                    sharks.append(players.pop())
+                    last_added = sharks
+                elif last_added == sharks:
+                    dragons.append(players.pop())
+                    last_added = dragons
+                elif last_added == dragons:
+                    raptors.append(players.pop())
+                    last_added = raptors
+            except IndexError as error:
+                pass
 
-    def team_writer(team, players):
-        '''Takes a list of players and their corresponding team name and
-        appends them to teams.txt
-        '''
-        with open('teams.txt', 'a') as file:
-            file.write('{}\n'.format(team))
-            for player in players:
-                file.write('{}, {}, {}\n'.format(player['Name'],
-                                                 player['Experienced'],
-                                                 player['Guardian Name(s)']))
-            file.write('\n')
 
-    # Initializes the players variable, reads the csv file
-    # and appends the list of players to the players variable.
+    sharks, dragons, raptors = [], [], []
+    global last_added
+    last_added = raptors
+    while experienced_players:
+        assign_team(experienced_players)
+    while inexperienced_players:
+        assign_team(inexperienced_players)
+    return sharks, dragons, raptors
+
+
+def team_writer(team, players):
+    '''Takes a list of players and their corresponding team name and
+    appends them to teams.txt
+    '''
+    with open('teams.txt', 'a') as file:
+        file.write('{}\n'.format(team))
+        for player in players:
+            file.write('{}, {}, {}\n'.format(player['Name'],
+                                             player['Experienced'],
+                                             player['Guardian Name(s)']))
+        file.write('\n')
+
+
+def csv_reader():
+    '''Initializes the players variable, reads the csv file
+    and appends the list of players to the players variable.
+    '''
     players = []
     with open('soccer_players.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
@@ -39,9 +57,13 @@ if __name__ == '__main__':
                             'Experienced': player['Soccer Experience'],
                             'Guardian Name(s)': player['Guardian Name(s)']
                             })
+    return players
 
-    # Initializes variables for experienced and inexperienced players then
-    # iterates through the list of players and assigns the players accordingly.
+
+def player_splitter(players):
+    '''Initializes variables for experienced and inexperienced players then
+    iterates through the list of players and assigns the players accordingly.
+    '''
     experienced_players = []
     inexperienced_players = []
     for player in players:
@@ -49,9 +71,18 @@ if __name__ == '__main__':
             experienced_players.append(player)
         else:
             inexperienced_players.append(player)
+    return experienced_players, inexperienced_players
 
+
+def main():
+    players = csv_reader()
+    experienced_players, inexperienced_players = player_splitter(players)
     sharks, dragons, raptors = team_splitter(experienced_players,
                                              inexperienced_players)
     team_writer('Sharks', sharks)
     team_writer('Dragons', dragons)
     team_writer('Raptors', raptors)
+
+
+if __name__ == '__main__':
+    main()
