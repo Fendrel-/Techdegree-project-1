1d5d25c3d87e8d4bf4ed85ffb88931774e1302a6
Dan Swift <dswift88@gmail.com>
2018-10-22 13:27:18 -0700

    Added 'welcome_letter' function.

--- league_builder.py
+++ league_builder.py
@@ -21,7 +21,6 @@ def team_splitter(experienced_players, inexperienced_players):
             except IndexError as error:
                 pass
 
-
     sharks, dragons, raptors = [], [], []
     global last_added
     last_added = raptors
@@ -34,23 +33,36 @@ def team_splitter(experienced_players, inexperienced_players):
 
 def team_writer(team, players):
     '''Takes a list of players and their corresponding team name and
-    appends them to teams.txt
+    appends them to teams.txt.
     '''
-    with open('teams.txt', 'a') as file:
+    with open('teams.txt', 'w') as file:
         file.write('{}\n'.format(team))
         for player in players:
             file.write('{}, {}, {}\n'.format(player['Name'],
                                              player['Experienced'],
                                              player['Guardian Name(s)']))
         file.write('\n')
-        
 
-def welcome_letter(team, players):
+
+def welcome_letter(team, players, first_practice):
     '''Takes a list of players and their corresponding team name and creates
-    a welcome letter to the parents in the form of a .txt file for each player
+    a welcome letter to the parents as a .txt file for each player.
     '''
     for player in players:
-        print(player['Name'])
+        name = player['Name'].lower().split()
+        name = '_'.join(name)
+        print(name)
+        with open('{}.txt'.format(name), 'w') as file:
+            file.write('Dear {},\n\n\n'.format(player['Guardian Name(s)']))
+            file.write('We are excited to announce the teams for this year\'s ')
+            file.write('soccer league!\n\nCongratulations! {} has been chosen'.format(player['Name']))
+            file.write(' to be part of\nTeam {} this year!\n\n'.format(team))
+            file.write('The team\'s first practice will be held on {}.\n'.format(first_practice))
+            file.write('We look forward to seeing you there! Here\'s to a safe and fun season!\n\n\n')
+            file.write('Sincerely,\n\n')
+            file.write('Dan\n')
+            file.write('League Coordinator')
+
 
 def csv_reader():
     '''Initializes the players variable, reads the csv file
@@ -81,7 +93,7 @@ def player_splitter(players):
     return experienced_players, inexperienced_players
 
 
-def main():
+if __name__ == '__main__':
     players = csv_reader()
     experienced_players, inexperienced_players = player_splitter(players)
     sharks, dragons, raptors = team_splitter(experienced_players,
@@ -89,10 +101,6 @@ def main():
     team_writer('Sharks', sharks)
     team_writer('Dragons', dragons)
     team_writer('Raptors', raptors)
-    welcome_letter('Sharks', sharks)
-    welcome_letter('Dragons', dragons)
-    welcome_letter('Raptors', raptors)
-
-
-if __name__ == '__main__':
-    main()
+    welcome_letter('Sharks', sharks, 'October 25th at 4:00 PM')
+    welcome_letter('Dragons', dragons, 'October 24th at 4:00 PM')
+    welcome_letter('Raptors', raptors, 'October 25th at 6:00 PM')
